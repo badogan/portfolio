79e16526b79c56c3797f13f8b5c22c3f4ac2e97e
Basri Dogan <yulthebad01@gmail.com>
2026-06-26 09:02:10 +0100

    feat: add BrowserRouter with / and /apps routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import BlogPosts from "./components/BlogPosts";
 import Contact from "./components/Contact";
+import AppLauncher from "./components/AppLauncher";
 
 function App() {
   return (
-    <React.Fragment>
-      <Hero />
-      <Projects />
-      <BlogPosts />
-      <Contact />
-    </React.Fragment>
+    <Router>
+      <Switch>
+        <Route exact path="/apps" component={AppLauncher} />
+        <Route path="/">
+          <React.Fragment>
+            <Hero />
+            <Projects />
+            <BlogPosts />
+            <Contact />
+          </React.Fragment>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
